571e9f467f93ee31885cf0145a112e6f26099c6c
Echo-73 <divijabellapukonda@gmail.com>
2025-03-11 11:13:25 +0530

    Update eventController.js

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -1,25 +1,3 @@
-// const Event = require("../models/Event");
-
-// exports.createEvent = async (req, res) => {
-//   try {
-//     const { name, dateTime } = req.body;
-//     const newEvent = new Event({ name, dateTime });
-//     await newEvent.save();
-//     res.status(201).json(newEvent);
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// };
-
-// exports.getEvents = async (req, res) => {
-//   try {
-//     const events = await Event.find().populate("participants");
-//     res.json(events);
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// };
-
 const Event = require("../models/Event");
 
 const addEvent = async (req, res) => {
