74fee33ba96ab2e402bf1a31302ae748bef1eb7b
Echo-73 <divijabellapukonda@gmail.com>
2025-03-11 11:16:10 +0530

    Update participantRoutes.js

--- server/routes/participantRoutes.js
+++ server/routes/participantRoutes.js
@@ -4,48 +4,6 @@ const Participant = require("../models/Participant");
 const Event = require("../models/Event");
 const sendReminderSMS = require("../services/twilioService");
 
-// // Get participants for a specific event
-// router.get("/event/:eventId", async (req, res) => {
-//     try {
-//       const { eventId } = req.params;
-//       const participants = await Participant.find({ event: eventId });
-//       res.status(200).json(participants);
-//     } catch (error) {
-//       res.status(500).json({ error: "Error fetching participants" });
-//     }
-//   });
-
-// // Add a Participant & Send SMS Reminder
-// router.post("/", async (req, res) => {
-//   try {
-//     const { name, phone, eventId } = req.body;
-
-//     // Validate event existence
-//     const event = await Event.findById(eventId);
-//     if (!event) {
-//       return res.status(404).json({ error: "Event not found" });
-//     }
-
-//     // Create participant
-//     const participant = new Participant({ name, phone, event: eventId });
-//     await participant.save();
-
-//     // Link participant to event
-//     await Event.findByIdAndUpdate(eventId, {
-//       $push: { participants: participant._id },
-//     });
-
-//     // Send SMS reminder
-//     await sendReminderSMS(phone, event.name, event.dateTime);
-
-//     res.status(201).json(participant);
-//   } catch (err) {
-//     res.status(400).json({ error: err.message });
-//   }
-// });
-
-// module.exports = router;
-
 // Add participant (ensure user owns the event)
 router.post("/", async (req, res) => {
   try {
